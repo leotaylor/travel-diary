d91f1e15d5b9168db84c4ca21193968f8ab30a95
Leo Taylor <leotaylor723@gmail.com>
2018-03-22 19:52:32 -0500

    layout

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("travel");
+const vacationArray = [
+    {
+        title: "Bahama",
+        imageUrl: "https://media.palladiumhotelgroup.com/kTCKtz6LFBOD-X5_4ilIzQ40sZo=/1280x768/smart/weddings2015/carouselcard/None_jamaica.jpg",
+        description: "by the sea"
+    },
+    {
+        title: "Jamaica",
+        imageUrl: "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5222667852001_5214854492001-vs.jpg?pubId=5104226627001&videoId=5214854492001",
+        description: "puff puff",
+    },
+    {
+        title: "Iceland",
+        imageUrl: "https://www.demilked.com/magazine/wp-content/uploads/2014/06/nordic-landscape-nature-photography-iceland-34.jpg",
+        description: "Vikings",
+    },
+    {
+        title: "Key Largo",
+        imageUrl: "https://www.twopeasandtheirpod.com/wp-content/uploads/2010/05/4592139475_5da1d9dba1.jpg",
+        description: "Key Lime Pie",
+    },
+    {
+        title: "Montego",
+        imageUrl: "https://www.riu.com/en/binaris/Montego_bay-02_tcm55-187573.jpg?v=10112017105255",
+        description: "whales",
+    },
+    {
+        title: "Swiss Alps",
+        imageUrl: "https://www.lamatracamagazine.com/wp-content/uploads/swiss-alps-day-trip-from.jpg",
+        description: "Ski Trip",
+    }
+];
